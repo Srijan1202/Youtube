53ce44ef492dedd080b514cbf1fca3e71372bd31
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-02-22 07:11:16 +0530

    api working

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { youtube } from '../utils/constants';
 
 const Videos = () => {
+
+  useEffect(() => {
+    getvideos();
+  },[])
+
+const getvideos = async () => {
+  const response = await fetch(youtube); 
+  const data = await response.json();
+  console.log(data);  
+}
+
+  
   return (
     <div>
       Videos
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+
+const google_api_key="AIzaSyBnXWoFDARz6taqJp6r8FWMcl-yawg4nnQ"
+
+export const youtube="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+google_api_key;
